0d1bd198f1dd6d4cfe278c85ccb7fd389cb8737e
pssw0728 <37687621+parksangsoo@users.noreply.github.com>
2023-07-30 16:38:31 +0900

    플러터 페이지 작업 공부

--- nomadcoder/flutter/toonflix/lib/main.dart
+++ nomadcoder/flutter/toonflix/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/Button.dart';
 
 class Player {
   String? name;
@@ -9,24 +10,27 @@ class Player {
 void main() {
   var nico = Player();
 
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
       backgroundColor: Colors.black,
       body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40),
-          child: Column(children: [
-            SizedBox(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const SizedBox(
               height: 80,
             ),
             Row(mainAxisAlignment: MainAxisAlignment.end, children: [
               Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-                Text("Hey, Selena",
+                const Text("Hey, Selena",
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 28,
@@ -38,7 +42,113 @@ class App extends StatelessWidget {
                       fontSize: 18,
                     )),
               ])
-            ])
+            ]),
+            const SizedBox(
+              height: 120,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Text(
+                  "Total Baleance",
+                  style: TextStyle(
+                      fontSize: 22, color: Colors.white.withOpacity(0.8)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            const Text(
+              "\$5 194 482",
+              style: TextStyle(
+                  fontSize: 44,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Button(
+                    text: "transfer",
+                    bgColor: Color(0xFFF1B33B),
+                    textColor: Colors.black),
+                Button(
+                    text: 'Request',
+                    bgColor: Color(0xFF1F2123),
+                    textColor: Colors.white),
+              ],
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            const Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("wallets",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600)),
+                Text("View all",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ))
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF1F2123),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 10,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Euro",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20)),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              Text("6428",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 10)),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text("EUR",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 10))
+                            ],
+                          ),
+                        ],
+                      ),
+                      Transform.scale(
+                          scale: 2.2,
+                          child: Transform.translate(
+                              offset: const Offset(-5, 12),
+                              child: const Icon(Icons.euro_rounded,
+                                  color: Colors.white, size: 88)))
+                    ],
+                  ),
+                )),
           ])),
     ));
   }
