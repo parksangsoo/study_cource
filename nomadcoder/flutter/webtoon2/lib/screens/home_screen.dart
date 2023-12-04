d402a78313bab4d6f7d3371347b7784042e19567
pssw0728 <37687621+parksangsoo@users.noreply.github.com>
2023-12-04 21:03:10 +0900

    패캠 강의 프로젝트 설치

--- nomadcoder/flutter/webtoon2/lib/screens/home_screen.dart
+++ nomadcoder/flutter/webtoon2/lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:webtoon2/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +19,19 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const Text("there is data!");
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(width: 20),
+            );
           }
-          return const Text("Loading");
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
