93d4948955a61b2222072c5874bfac87e6cd392b
pssw0728 <37687621+parksangsoo@users.noreply.github.com>
2023-12-05 22:23:37 +0900

    컴포넌트 제작

--- fastcampus/front_end_10/react-twitter/src/App.tsx
+++ fastcampus/front_end_10/react-twitter/src/App.tsx
@@ -1,22 +1,36 @@
-import './App.css';
-import { Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
+import { Route, Routes, Navigate, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home Page</h1>} />
-      <Route path="/posts" element={<h1>Post List Page</h1>} />
-      <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
-      <Route path="/posts/new" element={<h1>Post Detail Page</h1>} />
-      <Route path="/posts/edit/:id" element={<h1>Post Detail Page</h1>} />
-      <Route path="/profile" element={<h1>Post Detail Page</h1>} />
-      <Route path="/profile/edit" element={<h1>Post Detail Page</h1>} />
-      <Route path="/notifications" element={<h1>Post Detail Page</h1>} />
-      <Route path="/search" element={<h1>Post Detail Page</h1>} />
-      <Route path="/user/login" element={<h1>Post Detail Page</h1>} />
-      <Route path="/user/signUp" element={<h1>Post Detail Page</h1>} />
-      <Route path="*" element={ <Navigate replace to=""/>} />
-    </Routes>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/posts">Post List</Link>
+        </li>
+        <li>
+          <Link to="/posts/:id">Post Detail</Link>
+        </li>
+        <li>
+          <Link to="/posts/edit/:id">Post Edit</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/posts" element={<h1>Post List Page</h1>} />
+        <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
+        <Route path="/posts/new" element={<h1>Post New Page</h1>} />
+        <Route path="/posts/edit/:id" element={<h1>Post Detail Page</h1>} />
+        <Route path="/profile" element={<h1>Profile Page</h1>} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
   );
 }
 
